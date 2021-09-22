d37d82d16eae224fe2e18d380f3768cd40919ee4
82106 <pkr951113@gmail.com>
2021-09-22 14:59:03 +0900

    0922

---
+++ Level 2/구명보트.py
@@ -0,0 +1,15 @@
+# 0922
+# 구명보트
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    i, j = 0, len(people)-1
+    while i <= j:
+        answer += 1
+        if people[i] + people[j] <= limit:
+            i += 1
+        j -= 1
+    return answer
+
+print(solution([70, 50, 80, 50], 100))
+print(solution([70, 50, 80], 100))
---
+++ Level 2/큰 수 만들기.py
@@ -0,0 +1,21 @@
+# 0922
+# 큰 수 만들기
+# idea: 정렬
+from itertools import combinations
+
+def solution(number, k):
+    return sorted(["".join(cb) for cb in combinations(number, len(number)-k)])[-1]
+
+# idea: 스택
+def solution(number, k):
+    answer = []  # Stack
+
+    for num in number:
+        while k > 0 and answer and answer[-1] < num:
+            answer.pop()
+            k -= 1
+        answer.append(num)
+
+    return ''.join(answer[:len(answer) - k])
+print(solution("1924", 2))
+print(solution("4177252841", 4))
