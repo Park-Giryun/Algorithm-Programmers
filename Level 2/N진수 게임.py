bcded238517e0a14409cf65d9f39740a00a8c177
82106 <pkr951113@gmail.com>
2021-10-21 00:18:34 +0900

    1020

---
+++ Level 2/N진수 게임.py
@@ -0,0 +1,21 @@
+# 1020
+# N진수 게임
+# idea: 구현
+def convert(n, base):
+    arr = "0123456789ABCDEF"
+    q, r = divmod(n, base)
+    if q == 0:
+        return arr[r]
+    else:
+        return convert(q, base) + arr[r]
+
+def solution(n, t, m, p):
+    answer = ''
+    temp = ''
+    for i in range(t * m): # 모든 턴의 답
+        temp += convert(i, n)
+    for i in range(p - 1, t * m, m): # 튜브의 답
+        answer += temp[i]
+    return answer
+
+print(solution(2, 4, 2, 1))
--- Level 2/파일명 정리.py
+++ Level 2/파일명 정리.py
@@ -6,11 +6,10 @@ import re
 
 def solution(files):
     a = sorted(files, key=lambda file: int(re.findall('\d{1,5}', file)[0]))
-    b = sorted(a, key=lambda file: re.split('\d{1,5}', file.lower())[0])
+    b = sorted(a, key=lambda file: re.split('\d+', file.lower())[0])
     return b
 
 # 내가 푼 코드
-
 def solution(files):
     answer = []
     temp = []
@@ -32,5 +31,7 @@ def solution(files):
         answer.append(files[tmp[2]])
     return answer
 
-# print(solution(["foo9.txt", "foo010bar020.zip", "F-15"]))
 print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
+print(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]))
+
+# 회고: re, findall
