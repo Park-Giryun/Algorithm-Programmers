b4df29ce19b1ec8d71f28014fce65e31e6b93b04
82106 <pkr951113@gmail.com>
2021-12-14 22:06:05 +0900

    1214

---
+++ Level 3/네트워.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+
+    def bfs(a):
+        nonlocal n, computers, visited
+        q = deque([a])
+        while q:
+            x = q.popleft()
+            visited[x] = True
+            for y in range(n):
+                if y != x and computers[x][y] == 1 and not visited[y]:
+                    q.append(y)
+
+    for i in range(n):
+        if not visited[i]:
+            bfs(i)
+            answer += 1
+
+    return answer
+
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
+print(solution(5, [[1,1,1,0,0], [1,1,0,0,0], [1,0,1,0,0], [0,0,0,1,1], [0,0,0,1,1]]))
